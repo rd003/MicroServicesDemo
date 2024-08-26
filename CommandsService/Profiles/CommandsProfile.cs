017809ae6d3f8682c72248f9b25df6574547cab2
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-08-26 08:25:59 +0530

    final

--- CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CommandsService.Dtos;
 using CommandsService.Models;
+using PlatformService;
 // using PlatformService;
 
 namespace CommandsService.Profiles
@@ -15,10 +16,10 @@ namespace CommandsService.Profiles
             CreateMap<Command, CommandReadDto>();
             CreateMap<PlatformPublishedDto, Platform>()
                 .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.Id));
-            //CreateMap<GrpcPlatformModel, Platform>()
-            //     .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.PlatformId))
-            //     .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-            //     .ForMember(dest => dest.Commands, opt => opt.Ignore());
+            CreateMap<GrpcPlatformModel, Platform>()
+                .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.PlatformId))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Commands, opt => opt.Ignore());
         }
     }
 }
--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,6 +1,7 @@
 using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
+using CommandsService.SyncDataServices.Grpc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,7 +18,7 @@ builder.Services.AddHostedService<MessageBusSubscriber>();
 
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-// builder.Services.AddScoped<IPlatformDataClient, PlatformDataClient>();
+builder.Services.AddScoped<IPlatformDataClient, PlatformDataClient>();
 
 var app = builder.Build();
 
@@ -33,5 +34,7 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+PrepDb.PrepPopulation(app);
 
 app.Run();
